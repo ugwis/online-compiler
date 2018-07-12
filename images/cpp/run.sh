b42f2826f7653320aaf4843226c84af6723c8a4d
Yuta K <orn_tk@me.com>
2018-07-13 01:14:58 +0900

    Return added prefix e.g. stdout, stderr, gcc...etc

--- images/cpp/run.sh
+++ images/cpp/run.sh
@@ -1,9 +1,22 @@
-#!/bin/sh
-timeout 3 ./main
+#!/bin/bash
+g++ -std=c++1z -ggdb -o ./main ./main.c | awk'{print "g++:$0";fflush()}'
 status_code=$?
 if [ ${status_code} -ne 0 ];then
 	echo "Exit Code: ${status_code}"
+	exit 0
+fi
+mkfifo stdout
+mkfifo stderr
+(cat < stdout) | awk '{print "stdout:"$0 > "/dev/stdout";fflush()}'&
+(cat < stderr) | awk '{print "stderr:"$0 > "/dev/stdout";fflush()}'&
+timeout 3 ./main 1> stdout 2> stderr
+status_code=$?
+rm stdout&
+rm stderr&
+if [ ${status_code} -ne 0 ];then
+	echo "Exit Code: ${status_code}"
 	if [ ${status_code} -eq 139 ];then
-		gdb main core --batch
+		gdb main core --batch | awk '{print "gdb:$0";fflush()}'
 	fi
+	exit 0
 fi
